79a5174853b4d8369e25ee9167413796e9d4c90d
ElleryVernon <tmpks3@gmail.com>
2021-06-09 03:34:52 +0900

    create 01_nonBlockingIO.js

---
+++ 01_nonBlockingIO.js
@@ -0,0 +1,18 @@
+resources = [socketA, socketB, fileA]
+while (!resources.isEmpty()) {
+    for (resource of resources) {
+        // 읽기를 시도
+        data = resources.read()
+        if (data === NO_DATA_AVAILABLE) {
+            // 이 순간에는 읽을 데이터가 없음
+            continue
+        }
+        if (data === RESOURCE_CLOSED) {
+            // 리소스가 닫히고 리스트에서 삭제
+            resources.remove(i)
+        } else {
+            // 데이터 받고 처리
+            consumeData(data)
+        }
+    }
+}
