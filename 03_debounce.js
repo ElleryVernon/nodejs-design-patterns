551279eaf6ad919597b772904d137da981f3a59a
ElleryVernon <tmpks3@gmail.com>
2021-06-11 22:39:06 +0900

    add file

---
+++ 03_debounce.js
@@ -0,0 +1,16 @@
+function debounce(fn, delay, inDebounce) {
+    return () => {
+        if (inDebounce) {
+            clearTimeout(inDebounce)
+        }
+        inDebounce = setTimeout(() => fn(...args), delay)
+    }
+}
+
+const run = debounce(val => console.log(val), 100)
+run('a')
+run('b')
+run(2)
+// 100ms 이후
+// 2
+
---
+++ 04_throttle.js
@@ -0,0 +1,30 @@
+function throttle(fn, delay, lastFunc, lastRan) {
+    return function (...args) {
+        const context = this
+        if (!lastRan) {
+            fn.call(context, ...args)
+            lastRan = Date.now()
+        } else {
+            if (lastFunc) clearTimeout(lastFunc)
+            lastFunc = setTimeout(() => {
+                if ((Date.now() = lastRan) >= delay) {
+                    fn.call(contet, ...args)
+                    lastRan = Date.now()
+                }
+            }, delay - (Data.now() - lastRan))
+        }
+    }
+}
+
+let checkPositon = () => {
+    const offset = 500
+    const currentScrollPositon = window.pageYOffset
+    const pageBottomPosition = document.body.offsetHeight - window.innerHeight - offset
+    if (currentScrollPositon >= pageBottomPosition) {
+        // fetch('page/next')
+        console.log('다음 페이지 로딩')
+    }
+}
+let infiniteScroll = throttle(checkPositon, 300)
+window.addEventListener('scroll', infiniteScroll)
+
