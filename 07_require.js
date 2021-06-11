42a980373646e7525cb419ca4abb3c07e7035e05
Han SeongMin <83461987+ElleryVernon@users.noreply.github.com>
2021-06-11 23:35:50 +0900

    Update 07_require.js

--- 07_require.js
+++ 07_require.js
@@ -14,7 +14,8 @@ function loadModule(filename, module, require) {
 
 function require(moduleName) {
     console.log(`Require invoked fot module: ${moduleName}`)
-    // 모듈의 이름을 입력으로 받아 수행하는 첫 번째 일은 우리가 id라고 부르는 모듈의 전체경로를 알아내는(resolve) 것입니다. 이 작업은 이를 해결하기 위해 관련 알고리즘을 구현하고 있는 require.resolve()에 위임 됩니다.
+    // 모듈의 이름을 입력으로 받아 수행하는 첫 번째 일은 우리가 id라고 부르는 모듈의 전체경로를 알아내는(resolve) 것입니다.
+    // 이 작업은 이를 해결하기 위해 관련 알고리즘을 구현하고 있는 require.resolve()에 위임 됩니다.
     const id = require.resolve(moduleName)
     // 모듈이 이미 로드된 경우 캐시된 모듈을 사용합니다. 이 경우 즉시 반환합니다.
     if (require.cache[id]) {
@@ -22,7 +23,9 @@ function require(moduleName) {
     }
 
     // 모듈 메타데이터
-    // 모듈이 아직 로드되지 않은 경우 최초 로드를 위해서 환경을 설정합니다. 특히 빈 객체 리터럴을 통해 초기화된 exports 속성을 가지는 module 객체를 만듭니다. 이 객체는 불러온 모듈의 코드에서의 public API를 익스포트하는데 사용됩니다.
+    // 모듈이 아직 로드되지 않은 경우 최초 로드를 위해서 환경을 설정합니다.
+    // 특히 빈 객체 리터럴을 통해 초기화된 exports 속성을 가지는 module 객체를 만듭니다.
+    // 이 객체는 불러온 모듈의 코드에서의 public API를 익스포트하는데 사용됩니다.
     const module = {
         exports: {},
         id
@@ -33,7 +36,9 @@ function require(moduleName) {
     require.cache[id] = module
 
     // 모듈 로드
-    // 모듈 소스코드는 해당 파일에서 읽어오며, 코드는 앞에서 살펴본 방식으로 평가합니다. 방금 생성한 module 객체와 require() 함수의 참조를 모듈에 전달합니다. 모듈은 module.exports 객체를 조작하거나 대체하여 public API를 내보냅니다.
+    // 모듈 소스코드는 해당 파일에서 읽어오며, 코드는 앞에서 살펴본 방식으로 평가합니다.
+    // 방금 생성한 module 객체와 require() 함수의 참조를 모듈에 전달합니다.
+    // 모듈은 module.exports 객체를 조작하거나 대체하여 public API를 내보냅니다.
     loadModule(id, module, require)
     
     // 익스포트되는 변수 반환
